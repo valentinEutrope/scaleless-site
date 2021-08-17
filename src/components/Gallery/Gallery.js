53c02aa0062c57d06edccc1c0956abfa30cfdaa4
Eutrope Valentin <valentin.eutrope@free.fr>
2021-08-18 00:34:31 +0200

    implement gray filter and zoom effect

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -10,23 +10,51 @@ const IMAGES = [
   "https://www.fillmurray.com/320/470",
   "https://www.fillmurray.com/330/480",
   "https://www.fillmurray.com/340/490",
-  "https://www.fillmurray.com/525/355",
+  "https://www.fillmurray.com/565/355",
+  "https://www.fillmurray.com/575/355",
+  "https://www.fillmurray.com/585/355",
 ];
 
 const Gallery = () => {
   return (
     <div className="gallery">
       <div className="gallery__three-images">
-        <img className="gallery__image" src={IMAGES[0]} />
-        <img className="gallery__image" src={IMAGES[1]} />
-        <img className="gallery__image" src={IMAGES[2]} />
+        <div className="gallery__container">
+          <img className="gallery__image" src={IMAGES[0]} />
+        </div>
+        <div className="gallery__container">
+          <img className="gallery__image" src={IMAGES[1]} />
+        </div>
+        <div className="gallery__container">
+          <img className="gallery__image" src={IMAGES[2]} />
+        </div>
       </div>
 
       <div className="gallery__four-images">
-        <img className="gallery__image" src={IMAGES[3]} />
-        <img className="gallery__image" src={IMAGES[4]} />
-        <img className="gallery__image" src={IMAGES[5]} />
-        <img className="gallery__image" src={IMAGES[6]} />
+        <div className="gallery__container">
+          <img className="gallery__image" src={IMAGES[3]} />
+        </div>
+        <div className="gallery__container">
+          <img className="gallery__image" src={IMAGES[4]} />
+        </div>
+        <div className="gallery__container">
+          <img className="gallery__image" src={IMAGES[5]} />
+        </div>
+        <div className="gallery__container">
+          <img className="gallery__image" src={IMAGES[6]} />
+        </div>
+      </div>
+
+      <div className="gallery__three-images">
+        <div className="gallery__container">
+          <img className="gallery__image" src={IMAGES[7]} />
+        </div>
+        <div className="gallery__container">
+          <img className="gallery__image" src={IMAGES[8]} />
+        </div>
+        <div className="gallery__container">
+          <img className="gallery__image" src={IMAGES[9]} />
+        </div>
       </div>
     </div>
   );
