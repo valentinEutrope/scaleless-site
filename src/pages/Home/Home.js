50d8566c3a5cbbe39180806d667505a7f75371ba
valentinEutrope <valentin.eutrope@free.fr>
2023-03-17 09:59:08 +0100

    Refactor home sections

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,22 +7,43 @@ import Bio from "components/Bio";
 import Title from "components/Title";
 import images from "config/images.json";
 
+const Section = ({ title, children }) => {
+  return (
+    <div className="section">
+      <Title>{title}</Title>
+      {children}
+    </div>
+  );
+};
+
 const Home = () => {
+  const sections = [
+    {
+      title: "biographie",
+      content: <Bio />,
+    },
+    {
+      title: "galeries",
+      content: <Gallery images={images.mistrust} />,
+    },
+    {
+      title: "écoute",
+      content: <Listen />,
+    },
+    {
+      title: "évènements",
+      content: <></>,
+    },
+  ];
+
   return (
     <div className="home">
       <Introduction />
-      <div className="section">
-        <Title>Biographie</Title>
-        <Bio />
-      </div>
-      <div className="section">
-        <Title>Galeries</Title>
-        <Gallery images={images.mistrust} />
-      </div>
-      <div className="section">
-        <Title>Ecoute</Title>
-        <Listen />
-      </div>
+      {sections.map((section) => (
+        <Section key={`section-${section.title}`} title={section.title}>
+          {section.content}
+        </Section>
+      ))}
     </div>
   );
 };
