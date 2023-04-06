fc00cceccfd44923496b6e64338e432d654e76f4
valentinEutrope <valentin.eutrope@free.fr>
2023-04-06 21:19:01 +0200

    test

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer container">
-      <div>Footer</div>
+      <div>Scaleless</div>
     </footer>
   );
 };
